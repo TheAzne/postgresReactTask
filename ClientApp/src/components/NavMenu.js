b093e051d2e34fa40bf8102c5fd7337dc383d592
azne <a_kurbanov@hotmail.com>
2023-04-03 18:01:48 +0200

    update

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,45 +1,85 @@
-import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import './NavMenu.css';
+import React, { Component } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import "./NavMenu.css";
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
 
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.toggleDropdown = this.toggleDropdown.bind(this);
     this.state = {
-      collapsed: true
+      collapsed: true,
+      dropdownOpen: false,
     };
   }
 
-  toggleNavbar () {
+  toggleDropdown() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen,
+    }));
+  }
+
+  toggleNavbar() {
     this.setState({
-      collapsed: !this.state.collapsed
+      collapsed: !this.state.collapsed,
     });
   }
 
   render() {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">postgresReactTask</NavbarBrand>
+        <Navbar
+          className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3"
+          container
+          light
+        >
+          <NavbarBrand tag={Link} to="/">
+            postgresReactTask
+          </NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+          <Collapse
+            className="d-sm-inline-flex flex-sm-row-reverse"
+            isOpen={!this.state.collapsed}
+            navbar
+          >
             <ul className="navbar-nav flex-grow">
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                <NavLink tag={Link} className="text-dark" to="/">
+                  Home
+                </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/tasks">Tasks</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/tasks/create">Create task</NavLink>
+                <Dropdown
+                  isOpen={this.state.dropdownOpen}
+                  toggle={this.toggleDropdown}
+                >
+                  <DropdownToggle nav caret>
+                    Tasks
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem tag={Link} to="/tasks">
+                      View Tasks
+                    </DropdownItem>
+                    <DropdownItem tag={Link} to="/tasks/create">
+                      Create Task
+                    </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
               </NavItem>
             </ul>
           </Collapse>
